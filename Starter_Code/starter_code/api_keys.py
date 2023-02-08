781bcd48a70783468a404640980b16b929df0874
IsaacRooc <118013311+IsaacRooc@users.noreply.github.com>
2023-02-08 02:13:13 -0500

    this is it

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "a87689c78588fca8ebb8f328a2f31c0c"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "b2d30cebf4804beda9aff9bea36d761b"
